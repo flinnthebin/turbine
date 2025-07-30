0e8f244546dc557daa3d8f89de9b117b076fda98
chris flinn <christopher.flinn@gmail.com>
2025-07-30 19:16:12 +1000

    get file descriptor from device info

--- src/usb.rs
+++ src/usb.rs
@@ -1,5 +1,5 @@
 use nusb::transfer::{Bulk, ControlOut, ControlType, In, Out, Recipient};
-use nusb::{MaybeFuture, list_devices};
+use nusb::{Device, MaybeFuture, list_devices};
 use std::io::{Error, ErrorKind, Read, Write};
 use std::time::Duration;
 
@@ -9,7 +9,7 @@ struct UsbInfo {
 }
 
 #[allow(dead_code)]
-fn get_usb() -> Option<UsbInfo> {
+fn get_info() -> Option<UsbInfo> {
     let devices = list_devices().wait().ok()?;
     devices
         .filter(|dev| {
@@ -22,20 +22,35 @@ fn get_usb() -> Option<UsbInfo> {
         .next()
 }
 
+#[allow(dead_code)]
+fn get_descriptor(info: &UsbInfo) -> Option<Device> {
+    let mut devices = list_devices().wait().ok()?;
+    devices
+        .find(|dev| dev.vendor_id() == info.vid && dev.product_id() == info.pid)
+        .and_then(|dev| dev.open().wait().ok())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_usb() {
-        assert!(get_usb().is_some());
-    }
     /// Print all connected USB devices for manual debugging
     /// Run with: `cargo test -- --ignored --show-output`
     #[test]
+    #[ignore]
     fn output_devices() {
         let devices = list_devices().wait().expect("Some Failure");
         for device in devices {
             println!("{device:?}");
         }
+    }
+    #[test]
+    fn test_usb() {
+        assert!(get_info().is_some());
+    }
+    #[test]
+    /// Requires setting udev rules or sudo privileges to work
+    fn test_get_descriptor() {
+        let info = get_info().expect("Device not found");
+        assert!(get_descriptor(&info).is_some());
     }
 }
